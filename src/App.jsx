52932c9422fa6bea7385da57052012fd8b2a9bfe
TaddyRiv <taddymicheldriverosperez@gmail.com>
2025-04-28 11:12:13 -0400

    Actualización: cambios en CrearOrden y mejoras en UI

--- src/App.jsx
+++ src/App.jsx
@@ -16,89 +16,43 @@ import FinalizarCompra from "./pages/cliente/FinalizarCompra";
 import NotasVenta from "./pages/admin/NotasVenta";
 import OrdenesInternas from "./pages/admin/OrdenesInternas";
 import OrderDetails from "./pages/admin/OrderDetails";
-import PagoExitoso from "./pages/cliente/PagoExitoso"; // ✅ Importación corregida
+import PagoExitoso from "./pages/cliente/PagoExitoso";
+import CrearOrden from "./pages/admin/CrearOrden";
 
-// Cargar Stripe Promise con tu clave pública de Stripe
+// Cargar Stripe Promise
 const stripePromise = loadStripe("pk_test_51RHROzCrWG5rpklusUtAkQPaRU7wMVNVUIZ7baeX0czL8wBJHE59F1Qlw5WwzYSMzZn3a2VtpH6ys8mORxxnwuIT00VSqJYP4M");
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Envolvemos toda la App dentro de Elements */}
       <Elements stripe={stripePromise}>
         <Routes>
+
           {/* Rutas públicas */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
           {/* Rutas privadas */}
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
 
           {/* Rutas de administrador */}
-          <Route
-            path="/admin/usuarios"
-            element={
-              <PrivateRoute>
-                <Usuarios />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/admin/categorias"
-            element={
-              <PrivateRoute>
-                <Categorias />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/admin/productos"
-            element={
-              <PrivateRoute>
-                <Productos />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/admin/notas-venta" element={<NotasVenta />} />
-          <Route path="/admin/ordenes" element={<OrdenesInternas />} />
-          <Route path="/admin/detalles" element={<OrderDetails />} />
+          <Route path="/admin/usuarios" element={<PrivateRoute><Usuarios /></PrivateRoute>} />
+          <Route path="/admin/categorias" element={<PrivateRoute><Categorias /></PrivateRoute>} />
+          <Route path="/admin/productos" element={<PrivateRoute><Productos /></PrivateRoute>} />
+          <Route path="/admin/notas-venta" element={<PrivateRoute><NotasVenta /></PrivateRoute>} />
+          <Route path="/admin/ordenes" element={<PrivateRoute><OrdenesInternas /></PrivateRoute>} />
+          <Route path="/admin/detalles" element={<PrivateRoute><OrderDetails /></PrivateRoute>} />
+          <Route path="/admin/crear-orden" element={<PrivateRoute><CrearOrden /></PrivateRoute>} /> {/* ✅ Agregado */}
 
           {/* Rutas de cliente */}
-          <Route
-            path="/cliente/catalogo"
-            element={
-              <PrivateRoute>
-                <CatalogoCliente />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/cliente/carrito"
-            element={
-              <PrivateRoute>
-                <Carrito />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/cliente/finalizar"
-            element={
-              <PrivateRoute>
-                <FinalizarCompra />
-              </PrivateRoute>
-            }
-          />
+          <Route path="/cliente/catalogo" element={<PrivateRoute><CatalogoCliente /></PrivateRoute>} />
+          <Route path="/cliente/carrito" element={<PrivateRoute><Carrito /></PrivateRoute>} />
+          <Route path="/cliente/finalizar" element={<PrivateRoute><FinalizarCompra /></PrivateRoute>} />
 
           {/* Redirección luego de pago exitoso */}
           <Route path="/pago-exitoso" element={<PagoExitoso />} />
+          
         </Routes>
       </Elements>
     </BrowserRouter>
